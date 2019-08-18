71c93b89aa4f5d13b2bc34d084326dc6bed89f44
Andrew Landau <andrewtylerlandau@gmail.com>
2019-08-18 15:33:06 -0400

    scripts with simulations

--- caBuffering_RecreateExperimentalData/optimizeSystemParameters_NoEndogenous_Grid.m
+++ caBuffering_RecreateExperimentalData/optimizeSystemParameters_NoEndogenous_Grid.m
@@ -39,7 +39,7 @@ NT = length(tvec);
 fluorNames = {'fluo5f','ogb1-20然','ogb1-50然','ogb1-100然','fluo4-20然'};
 useBufferIdx = logical([0 1 1 1 1]);
 % fluorescent parameters
-fluorKon = 5e8;
+fluorKon = 5e10;
 fluorKd = [800e-9, 205e-9, 205e-9, 205e-9, 300e-9];
 fluorConcentrations = [300e-6, 20e-6, 50e-6, 100e-6, 20e-6];
 fluorPeak = [77e-9, 400e-9, 162.7e-9, 68.9e-9, 474.5e-9];
@@ -57,38 +57,25 @@ fluorPrms.tau = fluorTau;
 % fit parameters for decay - 
 fitPrms.startFitOffset = 0.005; % 5ms
 fitPrms.pkEndProportion = 0.1;
-fitPrms.numberSamples = 4;
 
 % optimizationParameters - Range
-NRest = 4;
-NBeta = 8;
+NRest = 8;
+NBeta = 4;
 NAmp = 4;
-NKon = 6;
-NKd = 8;
-NBt = 8;
 rest = linspace(40e-9,125e-9,NRest);
 beta = linspace(200,3000,NBeta);
-iAmp = logspace(-13,-10,NAmp);
-kon = logspace(5,11,NKon);
-kd = logspace(-8,-1,NKd);
-bt = logspace(-8,-1,NBt);
+iAmp = logspace(-12,-10,NAmp);
 
-costs = zeros(NRest,NBeta,NAmp,NKon,NKd,NBt);
-estimates = cell(NRest,NBeta,NAmp,NKon,NKd,NBt);
+costs = zeros(NRest,NBeta,NAmp);
+estimates = cell(NRest,NBeta,NAmp);
 parfor nbeta = 1:NBeta
     for nrest = 1:NRest
+        fprintf('Parfor Loop (%d) on nrest: %d/%d\n',nbeta,nrest,NRest);
         for namp = 1:NAmp
-            fprintf('Parfor Loop (%d) on nrest: %d/%d and namp: %d/%d\n',nbeta,nrest,NRest,namp,NAmp);
-            for nkon = 1:NKon
-                for nkd = 1:NKd
-                    for nbt = 1:NBt                        
-                        currentOptPrms = [rest(nrest) beta(nbeta) iAmp(namp) kon(nkon) kd(nkd) bt(nbt)]; %#ok - have to
-                        [currentCost,estimates{nrest,nbeta,namp,nkon,nkd,nbt}] = ...
-                            optimizeSystem(currentOptPrms,systemPrms,fluorPrms,odePrms,fitPrms,odeOptions);
-                        costs(nrest,nbeta,namp,nkon,nkd,nbt) = sum(currentCost.^2);
-                    end
-                end
-            end
+            currentOptPrms = [rest(nrest) beta(nbeta) iAmp(namp) kon(nkon) kd(nkd) bt(nbt)]; %#ok - have to
+            [currentCost,estimates{nrest,nbeta,namp}] = ...
+                optimizeSystem(currentOptPrms,systemPrms,fluorPrms,odePrms,fitPrms,odeOptions);
+            costs(nrest,nbeta,namp) = sum(currentCost.^2);
         end
     end
 end
@@ -98,31 +85,27 @@ end
 %% Report Best Parameters:
 currentOptPrms = struct();
 
-[~,costIdx] = sort(newCosts(:));
-[restIdx,betaIdx,ampIdx,konIdx,kdIdx,btIdx] = ind2sub(size(costs),costIdx);
-bestPrms = [rest(restIdx(1)) beta(betaIdx(1)) iAmp(ampIdx(1)) kon(konIdx(1)) kd(kdIdx(1)) bt(btIdx(1))];
+[~,costIdx] = sort(costs(:));
+[restIdx,betaIdx,ampIdx] = ind2sub(size(costs),costIdx);
+bestPrms = [rest(restIdx(1)) beta(betaIdx(1)) iAmp(ampIdx(1))];
 
 NO = sum(fluorPrms.useBufferIdx);
 objIdx = find(fluorPrms.useBufferIdx);
-bestSols = zeros(NT,3,NO);
+bestSols = zeros(NT,2,NO);
 for no = 1:NO
     cObjIdx = objIdx(no);
     currentOptPrms.rest= bestPrms(1);
     currentOptPrms.beta = bestPrms(2);
     currentOptPrms.iCalciumAmp = bestPrms(3);
-    currentOptPrms.kon = bestPrms(4);
-    currentOptPrms.kd = bestPrms(5);
-    currentOptPrms.bt = bestPrms(6);
 
     currentFluorPrms.kon = fluorPrms.kon; % For now this is a single value
     currentFluorPrms.kd = fluorPrms.kd(cObjIdx);
     currentFluorPrms.bt = fluorPrms.bt(cObjIdx);
 
-    cKappaEB = bestPrms(6) / (bestPrms(1) + bestPrms(5));
     cKappaFB = currentFluorPrms.bt / (bestPrms(1) + currentFluorPrms.kd);
-    cu0 = [bestPrms(1) bestPrms(1)*cKappaEB bestPrms(1)*cKappaFB];
+    cu0 = [bestPrms(1) bestPrms(1)*cKappaFB];
 
-    odeFunction = @(t,y) endogenousAndFluorophores(t,y,systemPrms,currentFluorPrms,currentOptPrms);
+    odeFunction = @(t,y) justFluorophores(t,y,systemPrms,currentFluorPrms,currentOptPrms);
     cSol = ode23s(odeFunction, odePrms.tspan, cu0, odeOptions);
     bestSols(:,:,no) = deval(cSol,tvec)';
 end
@@ -130,7 +113,7 @@ end
 % Make Report Legend
 reportLegend = cell(NO,1);
 for no = 1:NO
-    deltaFluor = bestSols(:,3,no) - bestSols(1,3,no);
+    deltaFluor = bestSols(:,2,no) - bestSols(1,2,no);
     [fluorMax,fluorPkIdx] = max(deltaFluor);
     fitStartIdx = find(tvec>= (tvec(fluorPkIdx)+fitPrms.startFitOffset),1,'first'); % start a little after peak
     fluorPkEndValue = fluorMax*fitPrms.pkEndProportion; % Value at which deltaFluor returned to some % of peak
@@ -147,33 +130,32 @@ end
 f = figure(170); clf;
 set(gcf,'units','normalized','outerposition',[0.1 0.1 0.7 0.4]);
 set(gcf,'DefaultAxesColorOrder',cat(2, linspace(0,1,NO)',zeros(NO,1),linspace(1,0,NO)'));
-xLimMax = [15 400 400];
+xLimMax = [50 400 400];
 
 names = {'Calcium','EndogenousBuffer','FluorescentBuffer'};
 legendNames = fluorNames(fluorPrms.useBufferIdx);
-for i = 1:3
-    subplot(1,3,i); hold on;
+for i = 1:2
+    subplot(1,2,i); hold on;
     plot(1000*tvec, 1e6*squeeze(bestSols(:,i,:)), 'linewidth', 1.5);
     xlim([0 xLimMax(i)]);
     xlabel('Time (ms)'); 
     ylabel('[X] (然)');
     title(names{i});
-    if i==1, legend(legendNames,'location','northeast'); end
+    if i==1
+        legend(legendNames,'location','northeast'); 
+    end
     set(gca,'fontsize',16);
     
-    if i==2
+    if i==1
         bestReport = {sprintf('Rest: %dnM',bestPrms(1)*1e9);
             sprintf('Beta: %d/s',bestPrms(2));
-            sprintf('iAmp: %dpA',bestPrms(3)*1e12);
-            sprintf('Kon: %d/M/s',bestPrms(4));
-            sprintf('Kd: %.1fmM',bestPrms(5)*1e6);
-            sprintf('[B]: %.1f然',bestPrms(6)*1e6);};
+            sprintf('iAmp: %dpA',bestPrms(3)*1e12)};
         XLIM = xlim;
         YLIM = ylim;
         text(mean(XLIM),mean(YLIM)+diff(YLIM)/5,bestReport);
     end
     
-    if i==3
+    if i==2
         %kappaFB = fluorPrms.bt(objIdx) ./ (bestPrms(1) + fluorPrms.kd(objIdx));
         %for no = 1:NO
         %    line([0 xLimMax(i)],bestSols(1,3,no)+fluorPrms.amp
@@ -201,9 +183,6 @@ function [cost,estimate] = optimizeSystem(optPrms,systemPrms,fluorPrms,odePrms,f
         currentOptPrms.rest= optPrms(1);
         currentOptPrms.beta = optPrms(2);
         currentOptPrms.iCalciumAmp = optPrms(3);
-        currentOptPrms.kon = optPrms(4);
-        currentOptPrms.kd = optPrms(5);
-        currentOptPrms.bt = optPrms(6);
         
         % Load in the current fluorophore
         currentFluorPrms.kon = fluorPrms.kon; % For now this is a single value
@@ -211,24 +190,23 @@ function [cost,estimate] = optimizeSystem(optPrms,systemPrms,fluorPrms,odePrms,f
         currentFluorPrms.bt = fluorPrms.bt(cObjIdx);
         
         % Compute kappas for initial state at the current [Ca]rest value
-        cKappaEB = optPrms(6) / (optPrms(1) + optPrms(5));
         cKappaFB = currentFluorPrms.bt / (optPrms(1) + currentFluorPrms.kd);
-        cu0 = [optPrms(1) optPrms(1)*cKappaEB optPrms(1)*cKappaFB];
+        cu0 = [optPrms(1) optPrms(1)*cKappaFB];
         
         % Parameterize ODE function and compute it
-        odeFunction = @(t,y) endogenousAndFluorophores(t,y,systemPrms,currentFluorPrms,currentOptPrms);
+        odeFunction = @(t,y) justFluorophores(t,y,systemPrms,currentFluorPrms,currentOptPrms);
         [cTime, cVals] = ode23s(odeFunction, odePrms.tspan, cu0, odeOptions);
         
         % Estimate Peak (from [CaB]/[Bt] as would be done in an experiment)
         % fractionBound * Kd / (1 - fractionBound) = Calcium --- (at eq.)
-        fractionRest = cVals(1,3)/currentFluorPrms.bt;
-        fractionPeak = max(cVals(:,3))/currentFluorPrms.bt;
+        fractionRest = cVals(1,2)/currentFluorPrms.bt;
+        fractionPeak = max(cVals(:,2))/currentFluorPrms.bt;
         estimateAtRest = fractionRest*currentFluorPrms.kd / (1 - fractionRest);
         estimateAtPeak = fractionPeak*currentFluorPrms.kd / (1 - fractionPeak);
         estimateValues(no,1) = estimateAtPeak - estimateAtRest; % delta calcium at peak
         
         % Estimate Time Constant from decay
-        deltaFluor = cVals(:,3) - cVals(1,3); % delta [CaB] over time
+        deltaFluor = cVals(:,2) - cVals(1,2); % delta [CaB] over time
         [fluorMax,fluorPkIdx] = max(deltaFluor); % max, max index
         fitStartIdx = find(cTime>= (cTime(fluorPkIdx)+fitPrms.startFitOffset),1,'first'); % start a little after peak
         fluorPkEndValue = fluorMax*fitPrms.pkEndProportion; % Value at which deltaFluor returned to some % of peak
@@ -243,26 +221,20 @@ function [cost,estimate] = optimizeSystem(optPrms,systemPrms,fluorPrms,odePrms,f
 end
 
 % ODE for endogenous + fluorescent buffer and optimization of system prms
-function dydt = endogenousAndFluorophores(t,y,systemPrms,fluorPrms,optPrms)
+function dydt = justFluorophores(t,y,systemPrms,fluorPrms,optPrms)
     % Calcium current stuff
     restCurrent = optPrms.beta * optPrms.rest;
     curr = restCurrent + ica(t,optPrms.iCalciumAmp)/(2*96485*systemPrms.v);
     extrusion = optPrms.beta * y(1);
 
-    % Endogenous Buffer Reaction 
-    endogAssociation = y(1)*(optPrms.bt - y(2))*optPrms.kon;
-    endogDissociation = y(2)*optPrms.kon*optPrms.kd;
-    endogExchange = endogAssociation - endogDissociation;
-
     % Fluorescent Buffer Reaction
-    fluorAssociation = y(1)*(fluorPrms.bt - y(3))*fluorPrms.kon;
-    fluorDissociation = y(3)*fluorPrms.kon*fluorPrms.kd;
+    fluorAssociation = y(1)*(fluorPrms.bt - y(2))*fluorPrms.kon;
+    fluorDissociation = y(2)*fluorPrms.kon*fluorPrms.kd;
     fluorExchange = fluorAssociation - fluorDissociation;
 
     %Output
-    dydt(1,1) = curr - extrusion - endogExchange - fluorExchange;
-    dydt(2,1) = endogExchange;
-    dydt(3,1) = fluorExchange;
+    dydt(1,1) = curr - extrusion - fluorExchange;
+    dydt(2,1) = fluorExchange;
 end
 
 
---
+++ caBuffering_RecreateExperimentalData/scaledKappa_diffusingFluorophore.m
@@ -0,0 +1,282 @@
+
+% - Fluorescent Buffer Parameteres come directly from Saba, 2002
+% - This is where I get values for Kd, 1/amp, tau, and [FB]
+% - Assuming all are diffusion limited (5e8/M/s)
+% - *** will eventually include a Mg term *** 
+% The fluo-5f tau and peak are guesses by measuring shit in fiji
+
+hpath = '/Users/landauland/Documents/Research/SabatiniLab/modeling/caBuffering_RecreateExperimentalData';
+fpath = fullfile(hpath,'figures');
+
+% Spine Parameters
+systemPrms.beta=1400.0;
+systemPrms.rest=5.0e-8;
+systemPrms.ks=20.0;
+systemPrms.v=(4/3)*pi*0.7^3*1e-15;
+iCalciumAmp = 7e-12; 
+
+u0 = systemPrms.rest;
+tspan = [0,0.5];
+dt = 0.000001;
+tvec = tspan(1):dt:tspan(2);
+
+% Instant Solution
+odeOptions = odeset('RelTol',1e-10,'AbsTol',1e-10);%,'MaxStep',dt);
+[trueTime,trueSol] = ode23s(@(t,y) instantBufferDynamics(t,y,systemPrms,iCalciumAmp),tspan,u0,odeOptions);
+plot(trueTime,trueSol);
+
+
+%% Optimize endogenous:[Kon,Kd,Bt], rest, beta, and iCalcium to get following results with fluorophores in the cell
+
+% ODE Parameters
+tspan = [0,1];
+odeOptions = odeset('RelTol',1e-10,'AbsTol',1e-10,'MaxStep',0.02);
+dt = 0.00001;
+tvec = tspan(1):dt:tspan(2);
+NT = length(tvec);
+
+spineVolume = 1e-15; % 1fL
+extrusionRate = 1500; % 1/s 
+restConcentration = 70e-9; % 70nM
+amplitude = 1e-12; % 7pA
+kappa = 20;
+kappaKD = 100;
+ 
+% On Rate of Fluorophore
+onRate = 5e8; 
+
+% Properties of Fluorophores
+ogbConcentration = 50e-6;% 50然 example
+ogbKD = 205e-9; % 205nM
+fluo5fConcentration = 100e-6; 
+fluo5fKD = 800e-9;
+fluo4ffConcentration = 100e-6;
+fluo4ffKD = 9e-6;
+
+useFluor = 'fluo4ff';
+fluorProperties.onRate = onRate;
+fluorProperties.totalConcentration = eval([useFluor,'Concentration']);
+fluorProperties.kd = eval([useFluor,'KD']);
+
+
+fluorConcentrations = 1e-6*(50:10:300);
+NC = length(fluorConcentrations);
+fluorKappa = @(fProp) fProp.totalConcentration / (restConcentration + fProp.kd);
+
+sols = cell(1,NC);
+values = zeros(NT,2,NC); 
+pkEstBuffer = zeros(NC,1);
+for i = 1:NC    
+    fluorProperties.totalConcentration = fluorConcentrations(i);
+    cKappa = fluorKappa(fluorProperties);
+    cInitialState = restConcentration*[1 cKappa];
+    prmODE = @(t,y) instantScaledPlusFluorophores(t,y,spineVolume,extrusionRate,restConcentration,amplitude,cKappa,kappaKD,fluorProperties);
+    sols{i} = ode23s(prmODE, tspan, cInitialState, odeOptions);
+    
+    values(:,:,i) = deval(sols{i},tvec)';
+    fOccupiedRest = values(1,2,i)/fluorProperties.totalConcentration;
+    fOccupiedPeak = max(values(:,2,i))/fluorProperties.totalConcentration;
+    estimateRest = fOccupiedRest*fluorProperties.kd / (1 - fOccupiedRest);
+    estimatePeak = fOccupiedPeak*fluorProperties.kd / (1 - fOccupiedPeak);
+    pkEstBuffer(i) = estimatePeak - estimateRest;
+end
+
+pkCalcium = max(squeeze(values(:,1,:)),[],1) - restConcentration; % Max of calcium transient
+
+%
+plotIdx = 1:3:NC;
+rMap = [linspace(0.2,1,length(plotIdx))',zeros(length(plotIdx),2)];
+figure(1); clf;
+set(gcf,'units','normalized','outerposition',[0.2 0.5 0.7 0.4]);
+set(gcf,'DefaultAxesColorOrder',rMap);
+
+subplot(1,3,1);
+plot(1000*tvec, 1e6*squeeze(values(:,1,plotIdx)), 'linewidth',1.5);
+xlim([0 25]);
+yLimCalcium = ylim;
+xlabel('Time (ms)');
+ylabel('然');
+title('[Ca^{2+}]');
+cb = colorbar;
+ylabel(cb,'[Fluor]');
+colormap(rMap);
+caxis(fluorConcentrations([1 end]));
+set(gca,'ColorScale','log');
+%legend(cellfun(@(c) sprintf('Kon: 1x10^{%d} /M/s',c),num2cell(log10(onRate)),'uni',0),'location','northeast');
+set(gca,'fontsize',16);
+
+subplot(1,3,2);
+fluorResponse = squeeze(values(:,2,:));
+dff = (fluorResponse - repmat(fluorResponse(1,:),NT,1)) ./ repmat(fluorResponse(1,:),NT,1);
+dffNorm = dff ./ repmat(max(dff,[],1),NT,1);
+
+toPlot = dff;
+plot(1000*tvec, 1e6*toPlot(:,plotIdx), 'linewidth',1.5);
+xlim([0 100]);
+yLimBuffer = ylim;
+xlabel('Time (ms)');
+ylabel('然');
+title('[CaFB]');
+set(gca,'fontsize',16);
+
+axes('Position',[0.535 0.65 0.08 0.25]);
+box on;
+plot(1000*tvec, 1e6*toPlot(:,plotIdx), 'linewidth',1.5);
+xlim([0 10]);
+%ylim([0 yLIM(2)]);
+xlabel('Time (ms)');
+ylabel('然');
+set(gca,'fontsize',12);
+
+
+subplot(1,3,3); hold on;
+plot(fluorConcentrations, 1e6*pkCalcium, 'linewidth',1,'marker','o','color','k');
+plot(fluorConcentrations, 1e6*pkEstBuffer, 'linewidth',1,'marker','o','color','r');
+% set(gca,'xscale','log');
+xlim(fluorConcentrations([1 end]));
+yLIM = ylim;
+% ylim([0 yLIM(2)]);
+xlabel('Kappa Kd');
+ylabel('[Ca^{2+}] (然)');
+title('FluorConcentration vs. \Delta[Ca^{2+}]');
+legend('TruePeak','Estimate','location','northeast');
+set(gca,'fontsize',16);
+
+
+
+
+%% ODE for 1 spine with fluorescent buffer only
+function dydt = justFluorophores(t,y,spineVolume,extrusionRate,restConcentration,amplitude,fluorProperties)
+    freeCalcium = y(1); 
+    boundBuffer = y(2); 
+    freeBuffer = fluorProperties.totalConcentration - boundBuffer;
+    
+    % Calcium current stuff
+    restCurrent = extrusionRate * restConcentration;
+    curr = restCurrent + ica(t,amplitude)/(2*96485*spineVolume);
+    extrusion = extrusionRate * freeCalcium; 
+
+    % Fluorescent Buffer Reaction
+    fluorAssociation = freeCalcium * freeBuffer * fluorProperties.onRate; 
+    fluorDissociation = boundBuffer * (fluorProperties.onRate * fluorProperties.kd);
+    
+    %Output
+    dydt(1,1) = curr - extrusion - fluorAssociation + fluorDissociation;
+    dydt(2,1) = fluorAssociation - fluorDissociation;
+end
+
+
+%% ODE for 1 spine with fluorescent buffer and instant buffer
+function dydt = instantPlusFluorophores(t,y,spineVolume,extrusionRate,restConcentration,amplitude,kappa,fluorProperties)
+    freeCalcium = y(1); 
+    boundBuffer = y(2); 
+    freeBuffer = fluorProperties.totalConcentration - boundBuffer;
+    
+    % Calcium current stuff
+    restCurrent = extrusionRate * restConcentration;
+    curr = restCurrent + ica(t,amplitude)/(2*96485*spineVolume);
+    extrusion = extrusionRate * freeCalcium; 
+
+    % Fluorescent Buffer Reaction
+    fluorAssociation = freeCalcium * freeBuffer * fluorProperties.onRate; 
+    fluorDissociation = boundBuffer * (fluorProperties.onRate * fluorProperties.kd);
+    
+    %Output
+    dydt(1,1) = curr - extrusion - fluorAssociation + fluorDissociation;
+    dydt(2,1) = fluorAssociation - fluorDissociation;
+    dydt = dydt/(1+kappa); % everything scaled by instant buffer
+end
+
+%% ODE for 1 spine with fluorescent buffer and instant buffer that scales kappa
+function dydt = instantScaledPlusFluorophores(t,y,spineVolume,extrusionRate,restConcentration,amplitude,kappa,kappaKD,fluorProperties)
+    freeCalcium = y(1); 
+    boundBuffer = y(2); 
+    freeBuffer = fluorProperties.totalConcentration - boundBuffer;
+    currentKappa = kappa * kappaKD/(freeCalcium + kappaKD);
+    
+    % Calcium current stuff
+    restCurrent = extrusionRate * restConcentration;
+    curr = restCurrent + ica(t,amplitude)/(2*96485*spineVolume);
+    extrusion = extrusionRate * freeCalcium; 
+
+    % Fluorescent Buffer Reaction
+    fluorAssociation = freeCalcium * freeBuffer * fluorProperties.onRate; 
+    fluorDissociation = boundBuffer * (fluorProperties.onRate * fluorProperties.kd);
+    
+    %Output
+    dydt(1,1) = (curr - extrusion + fluorDissociation - fluorAssociation)/currentKappa;
+    dydt(2,1) = fluorAssociation - fluorDissociation;
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
